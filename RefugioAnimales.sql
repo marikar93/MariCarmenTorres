1fae029d89cf0732441fc468208c9980e4cba813
marikar93 <44900756+marikar93@users.noreply.github.com>
2018-12-19 19:27:33 +0100

    Add files via upload
    Script limpio.

--- RefugioAnimales.sql
+++ RefugioAnimales.sql
@@ -46,7 +46,7 @@ CREATE TABLE `animales` (
   CONSTRAINT `FK_animales_sexo` FOREIGN KEY (`idsexo`) REFERENCES `sexo` (`idsexo`),
   CONSTRAINT `FK_animales_tipo` FOREIGN KEY (`idtipo`) REFERENCES `tipoanimal` (`idtipo`),
   CONSTRAINT `FK_animales_usuario` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -55,7 +55,6 @@ CREATE TABLE `animales` (
 
 LOCK TABLES `animales` WRITE;
 /*!40000 ALTER TABLE `animales` DISABLE KEYS */;
-INSERT INTO `animales` VALUES (14,'t','1',1,1,1,1,1,'2018-12-13',NULL),(15,'rony','12 años',1,3,11,2,2,'2018-12-13',NULL),(17,'Mari','25 años',0,2,3,1,1,'2018-12-13',NULL),(18,'Sansa','2 meses',1,1,1,1,1,'2015-03-15',1),(19,'Mar','1 año',1,1,1,1,1,'2016-05-01',2),(20,'ornitorrinco','7 meses',0,4,21,1,1,'2018-12-13',NULL),(21,'aa','a años',1,1,1,1,1,'2018-12-13',NULL),(22,'Mari','5 años',0,1,1,1,1,'2018-12-13',NULL),(23,'aa','a años',1,1,1,1,1,'2018-12-17',2),(25,'juan','a años',0,1,1,1,1,'2018-12-17',2),(26,'Mari','s',0,1,1,1,1,'2018-12-17',2),(27,'dssdf','sdf',0,1,1,1,1,'2018-12-17',2),(28,'dfsdffs','ssss',0,1,1,1,1,'2018-12-17',2),(29,'aaaaa','15',0,1,1,1,1,'2018-12-17',2),(30,'Mara','1 mes',0,1,1,1,1,NULL,NULL),(31,'roco','4 meses',1,1,1,1,1,NULL,NULL),(33,'layca','2 años',1,1,1,1,1,'2018-12-17',2),(34,'nora','15 dias',0,1,1,1,1,'2018-12-17',2);
 /*!40000 ALTER TABLE `animales` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,7 +78,6 @@ CREATE TABLE `color` (
 
 LOCK TABLES `color` WRITE;
 /*!40000 ALTER TABLE `color` DISABLE KEYS */;
-INSERT INTO `color` VALUES (1,'marron'),(2,'negro'),(3,'blanco'),(4,'gris');
 /*!40000 ALTER TABLE `color` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,7 +101,6 @@ CREATE TABLE `raza` (
 
 LOCK TABLES `raza` WRITE;
 /*!40000 ALTER TABLE `raza` DISABLE KEYS */;
-INSERT INTO `raza` VALUES (1,'angora'),(2,'bengala'),(3,'americano'),(4,'escoces'),(5,'siames'),(6,'egipcio'),(7,'persa'),(8,'tigre'),(9,'galgo'),(10,'terrier'),(11,'cocker'),(12,'pero de agua'),(13,'pastor aleman'),(14,'buldog frances'),(15,'chihuahua'),(16,'chow chow'),(17,'perro salchicha'),(18,'doberman'),(19,'husky siberiano'),(20,'yorsike'),(21,'sin raza');
 /*!40000 ALTER TABLE `raza` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -127,7 +124,6 @@ CREATE TABLE `sexo` (
 
 LOCK TABLES `sexo` WRITE;
 /*!40000 ALTER TABLE `sexo` DISABLE KEYS */;
-INSERT INTO `sexo` VALUES (1,'macho'),(2,'hembra');
 /*!40000 ALTER TABLE `sexo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -151,7 +147,6 @@ CREATE TABLE `tipoanimal` (
 
 LOCK TABLES `tipoanimal` WRITE;
 /*!40000 ALTER TABLE `tipoanimal` DISABLE KEYS */;
-INSERT INTO `tipoanimal` VALUES (1,'gato'),(2,'perro');
 /*!40000 ALTER TABLE `tipoanimal` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -175,7 +170,6 @@ CREATE TABLE `tipousuario` (
 
 LOCK TABLES `tipousuario` WRITE;
 /*!40000 ALTER TABLE `tipousuario` DISABLE KEYS */;
-INSERT INTO `tipousuario` VALUES (1,'administrador'),(2,'voluntario/adoptante');
 /*!40000 ALTER TABLE `tipousuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -201,7 +195,7 @@ CREATE TABLE `usuario` (
   UNIQUE KEY `user_UNIQUE` (`user`),
   KEY `FK_usuario_tipoUsuario_idx` (`idtipo`),
   CONSTRAINT `FK_usuario_tipoUsuario` FOREIGN KEY (`idtipo`) REFERENCES `tipousuario` (`idtipo`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -210,7 +204,6 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'marikar','marikar93','20225052w','Mari Carmen','marikar_93@hotmail.com',638162833,'c/Franz kafka',123456,1),(2,'maria','msperez','25635481x','Maria','mssibaja@hotmail.com',655485216,'c/la union',NULL,2),(3,'a','a','aaaaa','a','a',634561235,'a',NULL,2);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -223,4 +216,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-17 19:29:59
+-- Dump completed on 2018-12-19 19:20:35
